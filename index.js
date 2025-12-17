1d1de47e09be8282eaab117e1a0754fd66c685d6
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2025-12-17 17:44:59 +0000

    mispelled length and added mustache bracket

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ let quote = "...ask not what your country can do for you — ask what you can do
 
 //Add code below
 
- let quoteLength= quote.lenght;
+ let quoteLength= quote.length;
  let twentySecondLetter = quote[21];
- let output= `this is my string with ${quoteLength} and also [$twentySecondLetter}]`
+ let output= `this is my string with ${quoteLength} and also [${twentySecondLetter}]`
  
 
 document.getElementById("result").innerHTML = output;
