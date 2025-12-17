fa7e19f36957c97d19882a6b3ed5451dd0731210
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2025-12-17 16:07:37 +0000

    finished

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
 //Add code below
+
+let length = quotelength;
+let secondletter = quote;
+let quotelength = output;
+let output = [secondletter];
+
+document.getElementById("result").innerHTML = output
